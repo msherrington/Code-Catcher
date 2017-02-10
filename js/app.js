666d533fec6aa6e3405c55ebd5021b9267786c2a
Mark Sherrington <thewayforward3000@gmail.com>
2017-02-10 17:06:41 +0000

    basic functionality and images

--- js/app.js
+++ js/app.js
@@ -1,1 +1,73 @@
 console.log('JS loaded!');
+$(() => {
+
+  const $fallingDiv = $('#column1');
+  const $fallingDiv2 = $('#column2');
+  const $coder = $('#coder');
+
+
+  // .fadeIn()
+  function drop(){
+    $fallingDiv.animate({
+      top: 565
+    }, {
+      duration: 3000,
+      easing: 'linear',
+      complete: reset
+    });
+    $fallingDiv2.animate({
+      top: 565
+    }, {
+      duration: 3000,
+      easing: 'linear',
+      complete: reset
+    });
+  }
+
+
+
+  function reset() {
+    $(this).css({
+      top: -35
+    });
+    // drop();
+  }
+
+  drop();
+
+
+  // event listener for left/right arrow keys
+  $(document).on('keydown', (e) => {
+    switch (e.which) {
+      case 37:
+        $coder.stop().animate({
+          left: '-=100'
+        }); //left arrow key
+        break;
+      case 39:
+        $coder.stop().animate({
+          left: '+=100'
+        }); //right arrow key
+        break;
+    }
+  });
+
+
+
+//PSEUDO CODE
+
+// work out how to place falling divs in relation to codeWall
+
+//fade in falling objects
+//iterate through divs to decide which one starts falling when
+//location of div compared to top left corner of box
+//function to place image on falling div
+//function to decide whether div is "code or bug"
+
+//styling: codeWall image
+// falling code images
+// coder image!
+
+
+//bottom of DOM content loader
+});
