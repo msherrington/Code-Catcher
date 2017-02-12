db51ba433abfa8982308f5e55b6362d2624e5ed2
Mark Sherrington <thewayforward3000@gmail.com>
2017-02-12 00:01:09 +0000

    random images falling and coder dude's sliding improved

--- js/app.js
+++ js/app.js
@@ -1,47 +1,89 @@
 console.log('JS loaded!');
 $(() => {
 
-  const $falling1 = $('#column1');
-  // const $falling2 = $('#column2');
-  // const $falling3 = $('#column3');
-  // const $falling4 = $('#column4');
-  // const $falling5 = $('#column5');
-  // const $falling6 = $('#column6');
-  // const $falling7 = $('#column7');
-  // const $falling8 = $('#column8');
-  // const $falling9 = $('#column9');
-  // const $falling10 = $('#column10');
 
+
+  const $codewall = $('.codewall');
+
+  // code catcher dude
   const $coder = $('#coder');
 
-  function shake() {
-    $('#coder').effect( 'shake', {times: 1}, 200 );
-  }
+  // vars for falling divs
+  const $falling1 = $('#column1');
+  const $falling2 = $('#column2');
+  const $falling3 = $('#column3');
+  const $falling4 = $('#column4');
+  const $falling5 = $('#column5');
+  const $falling6 = $('#column6');
+  const $falling7 = $('#column7');
+  const $falling8 = $('#column8');
+  const $falling9 = $('#column9');
+  const $falling10 = $('#column10');
+
+  // array of falling divs, for random selection
+  const $fallingDivs = [ $falling1, $falling2, $falling3, $falling4, $falling5, $falling6, $falling7, $falling8, $falling9, $falling10 ];
+
+  // vars for images of falling divs
+  const $imageDrop1 = $('#drop1');
+  const $imageDrop2 = $('#drop2');
+  const $imageDrop3 = $('#drop3');
+  const $imageDrop4 = $('#drop4');
+  const $imageDrop5 = $('#drop5');
+  const $imageDrop6 = $('#drop6');
+  const $imageDrop7 = $('#drop7');
+  const $imageDrop8 = $('#drop8');
+  const $imageDrop9 = $('#drop9');
+  const $imageDrop10 = $('#drop10');
+
+  // array of falling image vars, for random selection
+  const $imageDrops = [ $imageDrop1, $imageDrop2, $imageDrop3, $imageDrop4, $imageDrop5, $imageDrop6, $imageDrop7, $imageDrop8, $imageDrop9, $imageDrop10 ];
+
+
+  let $randomDiv = null;
+  let $imageChoice = null;
+
+  //four functions for choosing img src
+  //put functions in array
+
+
+
+
+  // function random() {
+  // }
 
   // .fadeIn()
   function drop(){
-    $falling1.animate({
+    $randomDiv = Math.floor(Math.random()*10);
+    $imageChoice = Math.floor(Math.random()*12);
+
+    const $imagesLevel1 = [ html, bug, html, html, bug, html, html, bug, html, html, bug, html ];
+
+    function bug() {
+      $imageDrops[$randomDiv].attr('src', 'images/bug.png', 'class', 'bug');
+    }
+
+    function html() {
+      $imageDrops[$randomDiv].attr('src', 'images/html.png', 'class', 'code');
+    }
+
+    $imagesLevel1[$imageChoice]();
+    $fallingDivs[$randomDiv].animate({
       top: 565
     }, {
-      duration: 4500,
+      duration: 3000,
       easing: 'linear',
       complete: reset,
       progress: function() {
-        console.log($(this).position());
+        // console.log($(this).position());
         if (($(this).position().top > 530) && ($(this).position().top < 550) && (($coder.position().left >= -35) && ($coder.position().left <= 30))) {
           // alert('collide');
-          shake();
+          // shake();
+          // if id = bug, shake
         }
+        // reset div attributes
+        // hide it
       }
     });
-    // $falling2.animate({
-    //   top: 565
-    // }, {
-    //   duration: 3000,
-    //   easing: 'linear',
-    //   complete: reset
-    // });
-    // let top = $testObj.top
   }
 
   function reset() {
@@ -53,27 +95,60 @@ $(() => {
   drop();
 
 
-  // event listener for left/right arrow keys
-  $(document).on('keyup', (e) => {
-    switch (e.which) {
-      case 37:
-        $coder.stop().animate({
-          left: '-=60'
-        }, () => {
-          console.log($coder.position());
-        }); //left arrow key
-        break;
-      case 39:
-        $coder.stop().animate({
-          left: '+=60'
-        }, () => {
-          console.log($coder.position());
-        }); //right arrow key
-        break;
+
+  //testing new event listener for arrrow keys
+  var pressed = false;
+  $(document).on('keydown', (e) => {
+    if(!pressed){ //only start animation once
+      const width = $codewall.width();
+      switch (e.which) {
+        case 37:
+          $coder.stop().animate({
+            left: '-=' + width //allow the user the move the div over the whole doc
+          }, 3000); //left arrow key
+          break;
+        case 39:
+          $coder.stop().animate({
+            left: '+=' + width //allow the user the move the div over the whole doc
+          }, 3000); //left arrow key
+          break;
+      }
     }
+    pressed = true;
+  }).on('keyup', () => {
+    $coder.stop(); // stop the current animation
+    pressed = false;
   });
 
+  // OLD event listener for left/right arrow keys
+  // $(document).on('keyup', (e) => {
+  //   switch (e.which) {
+  //     case 37:
+  //       $coder.stop().animate({
+  //         left: '-=80'
+  //       }, () => {
+  //         console.log($coder.position());
+  //       }); //left arrow key
+  //       break;
+  //     case 39:
+  //       $coder.stop().animate({
+  //         left: '+=80'
+  //       }, () => {
+  //         console.log($coder.position());
+  //       }); //right arrow key
+  //       break;
+  //   }
+  // });
 
+
+
+  // function shake() {
+  //   $('#coder').effect( 'shake', {times: 1}, 200 );
+  // }
+
+  // $coder.on('change' (e) {
+  //   //event listener for when he's moving
+  // })
 
 
 //PSEUDO CODE
@@ -91,5 +166,38 @@ $(() => {
 // coder image!
 
 
-//bottom of DOM content loader
+
+//THIS IS THE BOTTOM OF THE DOM CONTENT LOADER
 });
+
+
+
+
+
+
+
+
+
+
+
+// // let imgValue = null;
+// function html(e) {
+//   $(e.target).attr('src', 'images/html.png');
+//   // imgValue = 'code';
+//   $(e.target).off();
+// }
+// // function css(e) {
+// //   $(e.target).attr('src', 'images/css.png');
+// //   // imgValue = 'code';
+// //   $(e.target).off();
+// // }
+// // function javascript(e) {
+// //   $(e.target).attr('src', 'images/javascript.png');
+// //   // imgValue = 'code';
+// //   $(e.target).off();
+// // }
+// function bug() {
+//   $(target).attr('src', 'images/bug.png');
+//   // imgValue = 'bug';
+//   $(target).off();
+// }
