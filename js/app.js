58cb2cb4aada881cfa876a5c8c0c634e9b32a5fb
Mark Sherrington <thewayforward3000@gmail.com>
2017-02-13 10:53:08 +0000

    image divs fade-in and start falling randomly

--- js/app.js
+++ js/app.js
@@ -9,79 +9,98 @@ $(() => {
   const $coder = $('#coder');
 
   // vars for falling divs
-  const $falling1 = $('#column1');
-  const $falling2 = $('#column2');
-  const $falling3 = $('#column3');
-  const $falling4 = $('#column4');
-  const $falling5 = $('#column5');
-  const $falling6 = $('#column6');
-  const $falling7 = $('#column7');
-  const $falling8 = $('#column8');
-  const $falling9 = $('#column9');
-  const $falling10 = $('#column10');
+  const $fallingDiv1 = $('#column1');
+  const $fallingDiv2 = $('#column2');
+  const $fallingDiv3 = $('#column3');
+  const $fallingDiv4 = $('#column4');
+  const $fallingDiv5 = $('#column5');
+  const $fallingDiv6 = $('#column6');
+  const $fallingDiv7 = $('#column7');
+  const $fallingDiv8 = $('#column8');
+  const $fallingDiv9 = $('#column9');
+  const $fallingDiv10 = $('#column10');
 
   // array of falling divs, for random selection
-  const $fallingDivs = [ $falling1, $falling2, $falling3, $falling4, $falling5, $falling6, $falling7, $falling8, $falling9, $falling10 ];
+  const $fallingDivs = [ $fallingDiv1, $fallingDiv2, $fallingDiv3, $fallingDiv4, $fallingDiv5, $fallingDiv6, $fallingDiv7, $fallingDiv8, $fallingDiv9, $fallingDiv10 ];
+
 
   // vars for images of falling divs
-  const $imageDrop1 = $('#drop1');
-  const $imageDrop2 = $('#drop2');
-  const $imageDrop3 = $('#drop3');
-  const $imageDrop4 = $('#drop4');
-  const $imageDrop5 = $('#drop5');
-  const $imageDrop6 = $('#drop6');
-  const $imageDrop7 = $('#drop7');
-  const $imageDrop8 = $('#drop8');
-  const $imageDrop9 = $('#drop9');
-  const $imageDrop10 = $('#drop10');
+  const $fallingImage1 = $('#drop1');
+  const $fallingImage2 = $('#drop2');
+  const $fallingImage3 = $('#drop3');
+  const $fallingImage4 = $('#drop4');
+  const $fallingImage5 = $('#drop5');
+  const $fallingImage6 = $('#drop6');
+  const $fallingImage7 = $('#drop7');
+  const $fallingImage8 = $('#drop8');
+  const $fallingImage9 = $('#drop9');
+  const $fallingImage10 = $('#drop10');
 
   // array of falling image vars, for random selection
-  const $imageDrops = [ $imageDrop1, $imageDrop2, $imageDrop3, $imageDrop4, $imageDrop5, $imageDrop6, $imageDrop7, $imageDrop8, $imageDrop9, $imageDrop10 ];
+  const $fallingImages = [ $fallingImage1, $fallingImage2, $fallingImage3, $fallingImage4, $fallingImage5, $fallingImage6, $fallingImage7, $fallingImage8, $fallingImage9, $fallingImage10 ];
 
 
-  let $randomDiv = null;
-  let $imageChoice = null;
+  //create variable ready for randomly selecting a falling div
+  let $randomArrayChoice = null;
+  //create variable ready for randomly selecting a falling image
+  let $imageFunctionChoice = null;
 
-  //four functions for choosing img src
-  //put functions in array
+  //start the game with all falling divs hidden
+  function hideFallingDivs() {
+    $fallingDiv1.hide();
+    $fallingDiv2.hide();
+    $fallingDiv3.hide();
+    $fallingDiv4.hide();
+    $fallingDiv5.hide();
+    $fallingDiv6.hide();
+    $fallingDiv7.hide();
+    $fallingDiv8.hide();
+    $fallingDiv9.hide();
+    $fallingDiv10.hide();
+  }
+  hideFallingDivs();
 
 
-
-
-  // function random() {
-  // }
-
-  // .fadeIn()
   function drop(){
-    $randomDiv = Math.floor(Math.random()*10);
-    $imageChoice = Math.floor(Math.random()*12);
+    //array of functions to assign image to falling div
+    const $imageFunctions = [ html, bug, html, html, bug, html, html, html, html, html, bug, html ];
 
-    const $imagesLevel1 = [ html, bug, html, html, bug, html, html, bug, html, html, bug, html ];
+    //generates random index of $fallingDivs and $fallingImages arrays
+    $randomArrayChoice = Math.floor(Math.random()*$fallingImages.length);
+    //generates random index of $imageFunctions array
+    $imageFunctionChoice = Math.floor(Math.random()*$imageFunctions.length);
 
+    //function assigns 'bug' image to falling div
     function bug() {
-      $imageDrops[$randomDiv].attr('src', 'images/bug.png', 'class', 'bug');
+      $fallingImages[$randomArrayChoice].attr('src', 'images/bug.png', 'class', 'bug');
     }
-
+    //function assigns 'html' image to falling div
     function html() {
-      $imageDrops[$randomDiv].attr('src', 'images/html.png', 'class', 'code');
+      $fallingImages[$randomArrayChoice].attr('src', 'images/html.png', 'class', 'code');
     }
 
-    $imagesLevel1[$imageChoice]();
-    $fallingDivs[$randomDiv].animate({
-      top: 565
+    //chooses random image function and runs it
+    $imageFunctions[$imageFunctionChoice]();
+
+      //chooses random div and shows (un-hides) it
+    // $fallingDivs[$randomArrayChoice].show();
+      //chooses random div and shows it with a slow fade-in
+    // $fallingDivs[$randomArrayChoice].fadeIn('slow');
+
+    //animates the random div, making it fall
+    $fallingDivs[$randomArrayChoice].fadeIn('slow').animate({
+      top: 500
     }, {
       duration: 3000,
       easing: 'linear',
       complete: reset,
       progress: function() {
         // console.log($(this).position());
-        if (($(this).position().top > 530) && ($(this).position().top < 550) && (($coder.position().left >= -35) && ($coder.position().left <= 30))) {
-          // alert('collide');
+        if (($(this).position().top > 360) && ($(this).position().top < 600) && ($coder.position().left > ($(this).position().left + 50)) && ($coder.position().left < ($(this).position().left - 35))) {
+          alert('collide');
           // shake();
           // if id = bug, shake
         }
-        // reset div attributes
-        // hide it
       }
     });
   }
@@ -90,9 +109,16 @@ $(() => {
     $(this).css({
       top: 0
     });
+    $(this).hide();
   }
 
-  drop();
+  function startGame() {
+    setInterval(() => {
+      drop();
+    }, 2000);
+  }
+
+  startGame();
 
 
 
@@ -103,11 +129,13 @@ $(() => {
       const width = $codewall.width();
       switch (e.which) {
         case 37:
+          console.log($coder.position());
           $coder.stop().animate({
             left: '-=' + width //allow the user the move the div over the whole doc
           }, 3000); //left arrow key
           break;
         case 39:
+          console.log($coder.position());
           $coder.stop().animate({
             left: '+=' + width //allow the user the move the div over the whole doc
           }, 3000); //left arrow key
@@ -120,6 +148,7 @@ $(() => {
     pressed = false;
   });
 
+
   // OLD event listener for left/right arrow keys
   // $(document).on('keyup', (e) => {
   //   switch (e.which) {
@@ -127,7 +156,6 @@ $(() => {
   //       $coder.stop().animate({
   //         left: '-=80'
   //       }, () => {
-  //         console.log($coder.position());
   //       }); //left arrow key
   //       break;
   //     case 39:
@@ -153,18 +181,14 @@ $(() => {
 
 //PSEUDO CODE
 
-// work out how to place falling divs in relation to codeWall
-
-//fade in falling objects
-//iterate through divs to decide which one starts falling when
+// work out how to place falling divs in relation to codeWall - DONE
+//fade in falling objects - DONE
+//iterate through divs to decide which one starts falling when - DONE
 //location of div compared to top left corner of box
-//function to place image on falling div
+//function to place image on falling div - DONE
 //function to decide whether div is "code or bug"
 
 //styling: codeWall image
-// falling code images
-// coder image!
-
 
 
 //THIS IS THE BOTTOM OF THE DOM CONTENT LOADER
